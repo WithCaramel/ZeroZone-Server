1b2c0666faa47d5ce6b23a3ff63b09b73207dd24
Mingadinga <minpearl0826@gmail.com>
2022-09-13 23:55:47 +0900

    develop:> Member Entity, Jpa Repository #6

---
+++ zero-zone/src/main/java/com/dalgona/zerozone/member/Member.java
@@ -0,0 +1,36 @@
+package com.dalgona.zerozone.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 300)
+    private String password;
+
+    @Column(length = 300, nullable = false)
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+}
