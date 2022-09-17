7c80bed4ca711f773dd4d0e43e963d0b2a4b721a
Mingadinga <minpearl0826@gmail.com>
2022-09-17 14:03:17 +0900

    add:> add validation in TokenRequestDto #10

--- zero-zone/src/main/java/com/dalgona/zerozone/security/dto/TokenRequestDto.java
+++ zero-zone/src/main/java/com/dalgona/zerozone/security/dto/TokenRequestDto.java
@@ -2,11 +2,19 @@ package com.dalgona.zerozone.security.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class TokenRequestDto {
+    @NotNull(message = "accessToken는 null일 수 없습니다.")
+    @NotEmpty(message = "accessToken는 빈값일 수 없습니다.")
     String accessToken;
+
+    @NotNull(message = "refreshToken는 null일 수 없습니다.")
+    @NotEmpty(message = "refreshToken는 빈값일 수 없습니다.")
     String refreshToken;
 
     @Builder
